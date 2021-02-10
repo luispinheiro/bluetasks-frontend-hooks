3be010875c03ee5b44c72d48d6b137635b91baa1
luispinheiro <lepinheiro100@terra.com.br>
2021-02-10 05:40:57 -0300

    aula 22.18 ok

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,5 @@ const SERVER = process.env.REACT_APP_SERVER;
 
 export const APP_NAME = "BlueTasks";
 export const AUTH_ENDPOINT = `${SERVER}`
-export const API_ENDPOINT = `${SERVER}/api` 
-export const JWT_TOKEN_NAME = "jwt";
+export const API_ENDPOINT = `${SERVER}/api`
 export const CREDENTIALS_NAME = "credentials";
